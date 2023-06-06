77f9664ccb92c34e1499f8d9c430dc447cabcf69
rhodinemma <rhodinemma10@gmail.com>
2023-06-06 15:59:47 +0300

    feat: simple aggregation for user stats

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,37 @@ const {
 const router = require("express").Router();
 
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = await User.find();
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
