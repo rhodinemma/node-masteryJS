bec0794b823af0adea11bd111f45679782834935
rhodinemma <rhodinemma10@gmail.com>
2023-06-06 12:47:53 +0300

    feat: add database connection

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-app.listen(4000, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 4000, () => {
   console.log("Server running");
 });
