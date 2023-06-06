86e7976e85e2c33826dff887c57c1c4187d0c1ce
rhodinemma <rhodinemma10@gmail.com>
2023-06-06 12:57:10 +0300

    chore: revise routing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const userRoute = require("./routes/user");
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +16,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server running");
