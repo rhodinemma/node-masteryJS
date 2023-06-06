d42c4f9f7d820a54bfd1a6bfbdefbab2777f037a
rhodinemma <rhodinemma10@gmail.com>
2023-06-06 15:43:30 +0300

    feat: retrieve single/all users

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,29 @@
 const User = require("../models/User");
-const { verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 const router = require("express").Router();
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const existingUser = await User.findById(req.params.id);
+    const { password, ...others } = existingUser._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
@@ -21,6 +43,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     );
 
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User deleted successfully!");
   } catch (err) {
     res.status(500).json(err);
   }
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -25,4 +25,18 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Forbidden!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
